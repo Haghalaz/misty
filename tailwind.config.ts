24b6a79c5f5f7d906632c9c2a7fc16356fa41ca7
Luiz Coelho <luiz.adalberto@selettra.com.br>
2024-07-10 18:07:46 -0300

    feat: Add new packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 
-const config: Config = {
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,13 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        timeBurner: ["var(--font-timeBurner)"],
+        sokBrubah: ["var(--font-sokBrubah)"],
       },
     },
   },
   plugins: [],
 };
-export default config;
